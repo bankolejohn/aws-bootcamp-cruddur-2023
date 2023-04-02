1a7dbcc24416b4742f5a6773135ec2ff9e38c2be
Bankole John <bankolejohn@gmail.com>
2023-04-02 01:42:31 +0000

    created new notification backend point

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -66,8 +66,8 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
-  @app.route("/api/activities/notifications", methods=['GET'])
-def data_home():
+@app.route("/api/activities/notifications", methods=['GET'])
+def data_notifications():
   data = NotificationsActivities.run()
   return data, 200
 
--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-class HomeActivities:
+class NotificationsActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
@@ -20,25 +20,7 @@ class HomeActivities:
         'replies_count': 0,
         'reposts_count': 0,
         'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-    },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
-      'message': 'I am out of prune juice',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': []
-    },
-    {
-      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
-      'created_at': (now - timedelta(hours=1)).isoformat(),
-      'expires_at': (now + timedelta(hours=12)).isoformat(),
-      'likes': 0,
-      'replies': []
+      }],    
     }
     ]
     return results
