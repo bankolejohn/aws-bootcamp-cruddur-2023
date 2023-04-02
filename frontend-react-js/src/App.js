2fa73661fb83a583b05f7988c5ffac7af8ee83e9
Bankole John <bankolejohn@gmail.com>
2023-04-02 02:05:40 +0000

    implement frontend notification page

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -21,8 +22,8 @@ const router = createBrowserRouter([
     element: <HomeFeedPage />
   },
   {
-    path: "/@:handle",
-    element: <UserFeedPage />
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   },
   {
     path: "/messages",
